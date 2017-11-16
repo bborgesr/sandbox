33c5c8728e67ab0291ef0a6a29664e5781065f93
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2017-11-16 06:12:51 +0000

    make app bookmarkable

--- app.R
+++ app.R
@@ -37,83 +37,94 @@ familify <- function(data, row, prev_family, isNamed) {
 
 mental_models <- familify(mental_models, row = 1, prev_family = NA, isNamed = TRUE)
 
-ui <- dashboardPage(skin = "yellow",
-  dashboardHeader(title = "Bar Bar Bar"),
-  dashboardSidebar(
-    sidebarMenu(id = "tabs",
-      menuItem("Home", tabName = "home", icon = icon("home")),
-      menuItem("Reference", icon = icon("book"), startExpanded = TRUE,
-        menuSubItem("Keyboard shortcuts - Inbox", 
-          tabName = "ks_inbox", selected = TRUE),
-        menuSubItem("Faux Braiding videos", 
-          tabName = "faux_braids"),
-        menuSubItem("Mental models table", 
-          tabName = "mental_models"),
-        menuSubItem("parabens", tabName = "parabens")
-      )
-    ),
-    conditionalPanel("input.tabs == 'mental_models'", {
-      selectInput("family", "Filter by family", unique(mental_models$family), 
-        unique(mental_models$family), multiple = TRUE)
-    })
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem("home", "Welcome home, Bárbara"),
-      tabItem("ks_inbox", 
-        helpText("From Inbox, click `Shift ?` to access shortcuts cheatsheet"),
-        hr(),
-        checkboxGroupInput("where", "Filter by area", inline = TRUE,
-          unique(ks$inbox$where), selected = unique(ks$inbox$where)),
-        tableOutput("reference_table")
-      ),
-      tabItem("faux_braids", 
-        helpText("Faux braiding haristyles! Yay!"),
-        hr(),
-        tags$iframe(
-          src = "https://www.youtube.com/embed/tWKZ6wg_bcg",
-          frameborder = "0", allowfullscreen = NA, width = "560", height = "315"
-        ),
-        tags$iframe(
-          src = "https://www.youtube.com/embed/iZdNH12TWWs",
-          frameborder = "0", allowfullscreen = NA, width = "560", height = "315"
-        ),
-        tags$iframe(
-          src = "https://www.youtube.com/embed/QqJKL5Ey7K8",
-          frameborder = "0", allowfullscreen = NA, width = "560", height = "315"
+ui <- function(req) {
+  dashboardPage(skin = "yellow",
+    dashboardHeader(title = "Bar Bar Bar"),
+    dashboardSidebar(
+      sidebarMenu(id = "tabs",
+        menuItem("Home", tabName = "home", icon = icon("home")),
+        menuItem("Reference", icon = icon("book"), startExpanded = TRUE,
+          menuSubItem("Keyboard shortcuts - Inbox", 
+            tabName = "ks_inbox", selected = TRUE),
+          menuSubItem("Faux Braiding videos", 
+            tabName = "faux_braids"),
+          menuSubItem("Mental models table", 
+            tabName = "mental_models"),
+          menuSubItem("parabens", tabName = "parabens")
         )
-      ), tabItem("mental_models", 
-        helpText("Mental mondels datatable"),
-        DT::dataTableOutput("mental_models")
       ),
-      tabItem("parabens", 
-        helpText("Para festas de aniversásrio"),
-        hr(),
-        p("Parabéns a você"),
-        p("Nesta data querida"),
-        p("Muitas felicidades"),
-        p("Muitos anos de vida"),
-        br(),
-        p("Hoje é dia de festa"),
-        p("Cantam as nossas almas"),
-        p("Para o(a) menino(a) ..."),
-        p("Uma salva de palmas!"),
-        br(),
-        p("Ele(a) hoje faz anos"),
-        p("Porque Deus assim quis"),
-        p("O que nós desejamos"),
-        p("É que seja feliz!"),
-        br(),
-        p("Tenha tudo de bom"),
-        p("Do que a vida contém"),
-        p("Tenha muita saúde"),
-        p("E amigos também")
+      conditionalPanel("input.tabs == 'mental_models'", {
+        selectInput("family", "Filter by family", unique(mental_models$family), 
+          unique(mental_models$family), multiple = TRUE)
+      })
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem("home", "Welcome home, Bárbara"),
+        tabItem("ks_inbox", 
+          helpText("From Inbox, click `Shift ?` to access shortcuts cheatsheet"),
+          hr(),
+          checkboxGroupInput("where", "Filter by area", inline = TRUE,
+            unique(ks$inbox$where), selected = unique(ks$inbox$where)),
+          tableOutput("reference_table")
+        ),
+        tabItem("faux_braids", 
+          helpText("Faux braiding haristyles! Yay!"),
+          hr(),
+          tags$iframe(
+            src = "https://www.youtube.com/embed/tWKZ6wg_bcg",
+            frameborder = "0", allowfullscreen = NA, width = "560", height = "315"
+          ),
+          tags$iframe(
+            src = "https://www.youtube.com/embed/iZdNH12TWWs",
+            frameborder = "0", allowfullscreen = NA, width = "560", height = "315"
+          ),
+          tags$iframe(
+            src = "https://www.youtube.com/embed/QqJKL5Ey7K8",
+            frameborder = "0", allowfullscreen = NA, width = "560", height = "315"
+          )
+        ), tabItem("mental_models", 
+          helpText("Mental mondels datatable"),
+          DT::dataTableOutput("mental_models")
+        ),
+        tabItem("parabens", 
+          helpText("Para festas de aniversásrio"),
+          hr(),
+          p("Parabéns a você"),
+          p("Nesta data querida"),
+          p("Muitas felicidades"),
+          p("Muitos anos de vida"),
+          br(),
+          p("Hoje é dia de festa"),
+          p("Cantam as nossas almas"),
+          p("Para o(a) menino(a) ..."),
+          p("Uma salva de palmas!"),
+          br(),
+          p("Ele(a) hoje faz anos"),
+          p("Porque Deus assim quis"),
+          p("O que nós desejamos"),
+          p("É que seja feliz!"),
+          br(),
+          p("Tenha tudo de bom"),
+          p("Do que a vida contém"),
+          p("Tenha muita saúde"),
+          p("E amigos também")
+        )
       )
     )
   )
-)
+}
 
 server <- function(input, output, session) {
+  observe({
+    # Trigger this observer every time an input changes
+    reactiveValuesToList(input)
+    session$doBookmark()
+  })
+  onBookmarked(function(url) {
+    updateQueryString(url)
+  })
+  
   output$reference_table <- renderTable({
     ks$inbox %>% 
       filter(where %in% input$where) %>% 
@@ -125,4 +136,4 @@ server <- function(input, output, session) {
   })
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server, enableBookmarking = "url")
